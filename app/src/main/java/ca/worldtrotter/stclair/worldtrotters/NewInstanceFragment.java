68236fa8bf1fe764d7df76b40c6488c420fd3176
Said Elshibiny <saidhassan.elshibiny01@stclairconnect.ca>
2018-03-31 18:33:34 -0400

    #7 created the NewInstanceFragment that acts as the newInstance that will allow to populate the ViewPager

--- app/src/main/java/ca/worldtrotter/stclair/worldtrotters/NewInstanceFragment.java
+++ app/src/main/java/ca/worldtrotter/stclair/worldtrotters/NewInstanceFragment.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link AboutUsFragment.OnFragmentInteractionListener} interface
+ * {@link NewInstanceFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link AboutUsFragment#newInstance} factory method to
+ * Use the {@link NewInstanceFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AboutUsFragment extends Fragment {
+public class NewInstanceFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -32,11 +29,9 @@ public class AboutUsFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public AboutUsFragment() {
+    public NewInstanceFragment() {
         // Required empty public constructor
     }
-
-    private CustomPagerAdapter customPagerAdapter;
 
     /**
      * Use this factory method to create a new instance of
@@ -44,11 +39,11 @@ public class AboutUsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment AboutUsFragment.
+     * @return A new instance of fragment NewInstanceFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AboutUsFragment newInstance(String param1, String param2) {
-        AboutUsFragment fragment = new AboutUsFragment();
+    public static NewInstanceFragment newInstance(String param1, String param2) {
+        NewInstanceFragment fragment = new NewInstanceFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -69,43 +64,13 @@ public class AboutUsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_about_us, container, false);
-        customPagerAdapter = new CustomPagerAdapter(getActivity().getSupportFragmentManager());
-        ViewPager viewPager = (ViewPager) view.findViewById(R.id.aboutViewPager);
-        viewPager.setAdapter(customPagerAdapter);
-
-
-        return view;
+        return inflater.inflate(R.layout.fragment_new_instance, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    /**
-     *
-     * @author Said Elshibiny
-     * Creating the CustomPagerAdapter for the ViewPager
-     */
-
-    public class CustomPagerAdapter extends FragmentPagerAdapter{
-
-        public CustomPagerAdapter(FragmentManager fm){
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            //Return 5 Pages
-            return 5;
         }
     }
 
