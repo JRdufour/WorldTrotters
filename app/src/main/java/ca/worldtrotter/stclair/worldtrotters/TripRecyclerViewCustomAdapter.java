1cdf8d91bd21667c9c7541e9ae6232ce99e94258
Said Elshibiny <saidhassan.elshibiny01@stclairconnect.ca>
2018-04-09 11:43:27 -0400

    #45 added the triple ellipse to the main recycler view with the edit item working

--- app/src/main/java/ca/worldtrotter/stclair/worldtrotters/TripRecyclerViewCustomAdapter.java
+++ app/src/main/java/ca/worldtrotter/stclair/worldtrotters/TripRecyclerViewCustomAdapter.java
@@ -6,9 +6,11 @@ import android.content.Context;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -44,20 +46,53 @@ public class TripRecyclerViewCustomAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
         //Set all the items in the customviewHolder
         final Trip currentTrip = tripList.get(position);
         ((CustomViewHolder) holder).tripName.setText(currentTrip.getName());
-        ((CustomViewHolder)holder).image.setOnClickListener(new View.OnClickListener() {
+//        ((CustomViewHolder)holder).image.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                android.support.v4.app.FragmentTransaction transaction = fm.beginTransaction();
+//                transaction.replace(R.id.main_content, TripFragment.newInstance(currentTrip.getTripID()));
+//                transaction.addToBackStack(null);
+//                transaction.commit();
+//            }
+//        });
+
+        ((CustomViewHolder) holder).menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                android.support.v4.app.FragmentTransaction transaction = fm.beginTransaction();
-                transaction.replace(R.id.main_content, TripFragment.newInstance(currentTrip.getTripID()));
-                transaction.addToBackStack(null);
-                transaction.commit();
+                //creating a popup menu
+                PopupMenu popup = new PopupMenu(context, ((CustomViewHolder) holder).menu);
+                //inflating menu from xml resource
+                popup.inflate(R.menu.mymenu);
+                //adding click listener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_edit:
+                                android.support.v4.app.FragmentTransaction transaction = fm.beginTransaction();
+                                transaction.replace(R.id.main_content, TripFragment.newInstance(currentTrip.getTripID()));
+                                transaction.addToBackStack(null);
+                                transaction.commit();
+                                break;
+                            case R.id.action_delete:
+                                //handle menu2 click
+                                break;
+                            case R.id.action_completed:
+                                //handle menu3 click
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+                //displaying the popup
+                popup.show();
             }
         });
-
     }
 
     @Override
@@ -72,12 +107,14 @@ public class TripRecyclerViewCustomAdapter extends RecyclerView.Adapter {
         protected TextView tripName;
         protected TextView photoAttribution;
         protected ImageView image;
+        protected ImageView menu;
 
         public CustomViewHolder(View view){
             super(view);
             this.tripName = view.findViewById(R.id.recycler_name);
             this.photoAttribution = view.findViewById(R.id.recycler_photo_attribution);
             this.image = view.findViewById(R.id.recycler_background_image);
+            this.menu = view.findViewById(R.id.optionsMenu);
         }
     }
 }
