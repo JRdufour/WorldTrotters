8e74561c2ea8012aeabf390c6c3b0c12a3320646
Said Elshibiny <saidhassan.elshibiny01@stclairconnect.ca>
2018-04-16 10:22:42 -0400

    #62 fixed issue with twitter

--- app/src/main/java/ca/worldtrotter/stclair/worldtrotters/TwitterFragment.java
+++ app/src/main/java/ca/worldtrotter/stclair/worldtrotters/TwitterFragment.java
@@ -4,12 +4,12 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.twitter.sdk.android.tweetui.TweetTimelineRecyclerViewAdapter;
 import com.twitter.sdk.android.tweetui.UserTimeline;
 
